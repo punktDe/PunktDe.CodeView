14077cc805dc3bcdb73b0441435373ec907501d9
Daniel Lienert <daniel@lienert.cc>
2016-03-22 21:35:35 +0100

    TASK: Initial commit

---
+++ Resources/Private/grunt/uglify.js
@@ -0,0 +1,16 @@
+module.exports = {
+    prism: {
+        src: [
+            'node_modules/prismjs/prism.js',
+
+            'node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js',
+            
+            'node_modules/prismjs/components/prism-bash.js',
+            'node_modules/prismjs/components/prism-javascript.js',
+            'node_modules/prismjs/components/prism-html.js',
+            'node_modules/prismjs/components/prism-php.js',
+            'node_modules/prismjs/components/prism-yaml.js'
+        ],
+        dest: '../Public/JavaScript/Prism.min.js'
+    }
+}
